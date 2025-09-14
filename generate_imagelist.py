ff715e597319811919a7198dd60f5d875e8ad31e
RayChien01 <jianr342@gmail.com>
2025-09-14 23:10:25 +0800

    Add files via upload

--- generate_imagelist.py
+++ generate_imagelist.py
@@ -5,7 +5,7 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 folder = os.path.join(base_dir, "images")  # images 在 rename.py 同一層
 output_file = os.path.join(base_dir, "imagelist.tex")
 
-print("圖片資料夾：", folder)
+print("image folder：", folder)
 
 with open(output_file, "w", encoding="utf-8") as f:
     for filename in sorted(os.listdir(folder)):
@@ -13,4 +13,4 @@ with open(output_file, "w", encoding="utf-8") as f:
             f.write(f"\\includegraphics[width=\\linewidth]{{images/{filename}}}\n")
             f.write("\\newpage\n")
 
-print(f"{output_file} 已經生成完成！")
+print(f"{output_file} imagelist.tex generated！")
